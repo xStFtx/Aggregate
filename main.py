82b3016887d1df99e54f528f23e0119460c07eee
xStFtx <you@example.com>
2023-10-16 11:59:20 +0200

    1.3

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 from qmath import Quaternion
 import nn
 
+class QuaternionDemo:
+    def __init__(self, q1, q2):
+        self.q1 = q1
+        self.q2 = q2
+
+    def display_basic_operations(self):
+        print(f"q1: {self.q1}")
+        print(f"q2: {self.q2}")
+        print(f"q1 * q2: {self.q1 * self.q2}")
+        print(f"q1 + q2: {self.q1 + self.q2}")
+
+    def display_angle_axis_representation(self, q):
+        angle, axis = q.to_angle_axis()
+        print(f"Angle-axis representation of {q}: Angle = {angle}, Axis = {axis}")
+
 def demonstrate_quaternion_operations():
     q1 = Quaternion(1, 2, 3, 4)
     q2 = Quaternion(2, 3, 4, 5)
-    print(f"q1: {q1}")
-    print(f"q2: {q2}")
-    
-    # Some basic operations for demonstration
-    print(f"q1 * q2: {q1 * q2}")
-    print(f"q1 + q2: {q1 + q2}")
-    angle, axis = q1.to_angle_axis()
-    print(f"Angle-axis representation of q1: Angle = {angle}, Axis = {axis}")
-    
+
+    demo = QuaternionDemo(q1, q2)
+    demo.display_basic_operations()
+    demo.display_angle_axis_representation(q1)
+    # Add more demonstrations as needed.
+
 def train_rl_model():
+    """Function to train RL model using Quaternion Neural Network."""
     nn.train_dqn()
 
 if __name__ == "__main__":
     print("Demonstrating Quaternion Operations:")
     demonstrate_quaternion_operations()
+    
     print("\nTraining RL Model using Quaternion Neural Network:")
     train_rl_model()
